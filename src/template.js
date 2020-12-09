27e84daa9d30c84764a624d9191f49695993bd3f
DouglasEde <briancarter340@gmail.com>
2020-12-08 17:13:27 -0800

    fixed deployment problem, and refractored template.js to be JSX expressions

--- src/template.js
+++ src/template.js
@@ -1,33 +1,35 @@
 
 
 const loginPage = () => {
-    return ` <h1>I.I.R.</h1>
+    return  (
         <section class='start-page'>
-            <label for='userName'>User Name:<label>
-            <input type='text' id='userName' placeholder='Enter User Name...' required>
-            <br>
-            <label for='password'>Password:<label>
-            <input type='text' id='password' placeholder='Enter Password' required>
-            <br>
+            <h1>I.I.R.</h1>
+            <label for='userName'>User Name:</label>
+            <input type='text' id='userName' placeholder='Enter User Name...' required />
+            <br />
+            <label for='password'>Password:</label>
+            <input type='text' id='password' placeholder='Enter Password' required />
+            <br />
             <button type='submit' id='submit'>Submit</button>
             <button type='button' id='cancel'>Cancel</button>
-        </section>`;
+        </section>);
 };
 
 const newUserForm = () => {
-    return `<h1>I.I.R.</h1>
+    return (
     <form id='newUserInfo'>
+        <h1>I.I.R.</h1>
         <section class='newUserForm'>
-        <label for='email'>Email:<label>
-        <input type='text' id='email' placeholder='Enter Email' required>
-        <label for='userName'>User Name:<label>
-        <input type='text' id='userName' placeholder='Enter User Name' required>
-        <label for='password'>Password:<label>
-        <input type='text' id='password' placeholder='Enter Password' required>
-        <label for='passwordConfirm'>Confirm Password:<label>
-        <input type='text' id='passwordConfirm' placeholder='Confirm Password' required>
+        <label for='email'>Email:</label>
+        <input type='text' id='email' placeholder='Enter Email' required />
+        <label for='userName'>User Name:</label>
+        <input type='text' id='userName' placeholder='Enter User Name' required />
+        <label for='password'>Password:</label>
+        <input type='text' id='password' placeholder='Enter Password' required />
+        <label for='passwordConfirm'>Confirm Password:</label>
+        <input type='text' id='passwordConfirm' placeholder='Confirm Password' required />
         </section>
-    </form>`;
+    </form>);
 }
 
 
